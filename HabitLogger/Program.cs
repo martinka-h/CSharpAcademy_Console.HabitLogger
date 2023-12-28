c93dc345850ecc2fc39813e3bd9837c08236bf40
martinka-h <132383732+martinka-h@users.noreply.github.com>
2023-12-28 12:05:53 +0200

    created database & table

---
+++ HabitLogger/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source = habitlogger.db";
+
+using (var connection = new SqliteConnection(connectionString))
+{
+    connection.Open();
+    var tableCmd = connection.CreateCommand();
+
+    tableCmd.CommandText =
+        @"CREATE TABLE IF NOT EXISTS drinking_water (
+Id INTEGER PRIMARY KEY AUTOINCREMENT,
+DATE TEXT,
+Quantity INTEGER)";
+
+    tableCmd.ExecuteNonQuery();
+    connection.Close();
+}
